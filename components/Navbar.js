eb674331fc8fa9dfd4fcbd161275401cbf4028fc
Md Salman <md.salman@northsouth.edu>
2021-08-25 22:54:44 +0600

    Topbar-solve responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,26 +5,27 @@ import { FiHeart, FiPhoneCall, FiShoppingCart, FiUser } from 'react-icons/fi'
 import { CSSTransition } from 'react-transition-group'
 import styles from '../styles/Navbar.module.css'
 import CartSidePanel from './CartSidePanel'
+
 const Navbar = () => {
   const [openPanel, setOpenPanel] = useState(false)
 
-  const openPanelHandler = (e) => {
-    setOpenPanel(e)
+  const openPanelHandler = (event) => {
+    setOpenPanel(event)
   }
 
   return (
     <div className={styles.top__bar}>
-      <Container>
+      <Container fluid ="lg">
         <Row>
-          <Col lg={8} md={4} className="text-muted d-flex align-items-center">
+          <Col lg={8} md={4} sm={5} className="text-muted d-flex align-items-center">
             <a
               href="tel:+8801521109830"
-              className="text-decoration-none text-muted d-none d-md-block mx-2 "
+              className="text-decoration-none text-muted d-none d-sm-block mx-2 "
             >
               <FiPhoneCall /> (+88) 018-18058362
             </a>
           </Col>
-          <Col lg={4} md={8} className="d-flex justify-content-end">
+          <Col lg={4} md={8} sm={7} className="d-flex justify-content-end pe-0">
             <Link href="/wishList">
               <a className="text-decoration-none text-muted mx-2 d-flex align-items-center">
                 <FiHeart /> <span className="mx-1">Wish List</span>
@@ -36,7 +37,7 @@ const Navbar = () => {
               </a>
             </Link>
             <a
-              className="text-decoration-none bg-dark text-white mx-2 p-2"
+              className="text-decoration-none bg-dark text-white p-2"
               style={{ cursor: 'pointer' }}
               onClick={() => setOpenPanel(true)}
             >
